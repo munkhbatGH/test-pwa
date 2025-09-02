d827c2b05f75ef9912a8f6ee9afee8cc9d936d7b
ssmunkhbat <47767779+ssmunkhbat@users.noreply.github.com>
2025-09-02 17:08:32 +0800

    cccc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/test-pwa/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png" />
         <meta name="theme-color" content="#e0c8fd" />
 
